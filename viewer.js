8cc7d4b7565f5470e15aef01caa6a62dd834694b
Roland Rytz <roland.rytz@gmail.com>
2025-02-20 22:59:32 +0100

    add broken rendering

--- viewer.js
+++ viewer.js
@@ -81,6 +81,8 @@ function processFile(file){
 	processBuffers();
 	processBufferViews();
 	processImages();
+	processMeshes();
+	displayMeshes();
 	
 	setTimeout(function(){
 		loadingStatus.className = "fading";
@@ -97,7 +99,6 @@ function processBuffers(){
 }
 
 function processBufferViews(){
-	// not implemented here: byteStride
 	for(let i in glb.bufferViews){
 		let bv = glb.bufferViews[i];
 		bv.view = new DataView(glb.buffers[bv.buffer].bufferData, bv.byteOffset, bv.byteLength);
@@ -105,12 +106,92 @@ function processBufferViews(){
 }
 
 function processImages(){
-	for(var img of glb.images){
-		let imageData = new Blob([glb.bufferViews[img.bufferView].view], {type: img.mimeType});
-		let imageUrl = URL.createObjectURL(imageData);
-		let imgEl = new Image;
-		imagesContainer.appendChild(imgEl);
-		imgEl.src = imageUrl;
-		img.image = imgEl;
+	if(glb.images){
+		for(let img of glb.images){
+			let imageData = new Blob([glb.bufferViews[img.bufferView].view], {type: img.mimeType});
+			let imageUrl = URL.createObjectURL(imageData);
+			let imgEl = new Image;
+			imagesContainer.appendChild(imgEl);
+			imgEl.src = imageUrl;
+			img.image = imgEl;
+		}
+	}
+}
+
+function processMeshes(){
+	if(glb.meshes){
+		for(let mesh of glb.meshes){
+			for(let p in mesh.primitives){
+				let positionAccessor = glb.accessors[mesh.primitives[p].attributes.POSITION];
+				//console.log(positionAccessor);
+				if(positionAccessor.componentType == 5126 && positionAccessor.type.toUpperCase() == "VEC3"){
+					let bufferView = glb.bufferViews[positionAccessor.bufferView];
+					let view = bufferView.view;
+					let byteStride = 4; // 4 bytes for float32
+					if(bufferView.byteStride){
+						byteStride = bufferView.byteStride;
+					}
+					let count = positionAccessor.count;
+					
+					let positions = [];
+					
+					for(let i = 0; i < count; i++){
+						positions.push([]);
+						for(let c = 0; c < 3; c++){
+							positions[i][c] = view.getFloat32(i*byteStride+c*4, true);
+						}
+					}
+					
+					mesh.primitives[p].positions = positions;
+					
+					//console.log(positions);
+					
+				} else {
+					console.log("Accessor type not implemented yet: "+positionAccessor.type +", "+positionAccessor.componentType);
+				}
+			}
+		}
+	}
+}
+
+function displayMeshes(){
+	const canvasContainer = document.getElementById("canvasContainer");
+	const canvas = document.getElementById("renderCanvas");
+	const context = canvas.getContext("2d");
+	canvas.width = canvasContainer.clientWidth;
+	//canvas.height = canvasContainer.clientHeight;
+	canvas.height = canvas.width;
+	context.strokeStyle = "#fff";
+	
+	if(glb.meshes){
+		for(let mesh of glb.meshes){
+			for(let p in mesh.primitives){
+				let positions = mesh.primitives[p].positions;
+				if(positions){
+					for(let i = 0; i < positions.length; i += 3){
+						if(!positions[i+3]){
+							break;
+						}
+						let triangle = [];
+						for(let v = 0; v < 3; v++){
+							let x = positions[i+v][0];
+							let y = positions[i+v][1];
+							let z = positions[i+v][2];
+							
+							x = (x + 1.5)/3*canvas.width;
+							y = (y + 1.5)/3*canvas.height;
+							
+							triangle[v] = [x, y];
+						}
+						context.moveTo(triangle[0][0], triangle[0][1]);
+						context.lineTo(triangle[1][0], triangle[1][1]);
+						context.lineTo(triangle[2][0], triangle[2][1]);
+						context.closePath();
+						context.stroke();
+						console.log(triangle);
+					}
+				}
+			}
+		}
 	}
 }
